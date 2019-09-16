0bfff601a71542b445c75bffabbbffd09f931e55
Keri Deemer <kerid18@att.net>
2019-09-16 00:47:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def line(array)
+  if array == []
+    puts "The line is currently empty."
+  else
+    line_msg = ""
+    array.each_with_index do |customer, index|
+      line_msg << " #{index+1}. #{customer}"
+    end
+    puts "The line is currently:#{line_msg}"
+ end
+end
+    
+    

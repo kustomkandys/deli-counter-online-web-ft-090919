9b996d4f30addd7fbf453915d026c6ed5ccd7677
Keri Deemer <kerid18@att.net>
2019-09-16 01:00:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,6 +22,7 @@ def now_serving(array)
   if array == []
     puts "There is nobody waiting to be served!"
   elsif array.length += 1 
-    puts "Currently serving #{array[0]"
+    puts "Currently serving #{array[0]}"
+    array.shift
 end
     
